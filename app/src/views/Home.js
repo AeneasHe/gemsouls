76b449880fd489b54f0589467cf1a1dd3906497e
AeneasHe <thhk06@gmail.com>
2021-08-22 06:32:56 +0800

    调整路由添加样式

---
+++ app/src/views/Home.js
@@ -0,0 +1,25 @@
+
+
+function Home() {
+    return (
+        <div className="container">
+
+            <div className="flex justify-center ">
+                <h3>Gemsouls</h3>
+            </div>
+
+            <div className="flex h-3/4   flex-col justify-center">
+
+                <div className="flex  place-content-center text-center" >
+
+                    <div className="bg-blue-400 text-white box-border  p-1 w-max h-min text-center">
+                        <a href="/chatroom" > go to chatroom  </a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default Home;
